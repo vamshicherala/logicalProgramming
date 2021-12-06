9c735d6930926dc0d4c9e51dd5be7144aef26400
vamshicherala <vamshikrishna199904@gmail.com>
2021-12-06 06:54:01 +0530

    Added:Program to compute fibonacci series

---
+++ src/com/logical/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.logical;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of n :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int n1=0,n2=1,n3;
+        System.out.print(n1+" "+n2);
+        for (int i=2;i<n;i++){
+            n3 = n1+n2;
+            System.out.print(" "+n3);
+             n1=n2;
+             n2=n3;
+        }
+    }
+}
